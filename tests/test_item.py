5f8354526422191e329606f864d82d2d4c240607
user <aytem95@gmail.com>
2023-05-02 00:49:39 +0600

    add_tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -18,24 +18,3 @@ def test_apply_discount():
 
     assert item1.price == 8000.0
     assert item2.price == 20000
-
-
-# from src.item import Item
-#
-#
-# def test_get_total_price():
-#     item = Item("Test item", 10.0, 5)
-#     assert item.get_total_price() == 50.0
-#
-#
-# def test_apply_discount():
-#     item = Item("Test item", 10.0, 5)
-#     item.apply_discount(0.2)  # apply 20% discount
-#     assert item.price == 8.0
-#     assert item.discount_level == 0.2
-#
-#
-# def test_apply_discount_invalid(pytest=None):
-#     item = Item("Test item", 10.0, 5)
-#     with pytest.raises(ValueError):
-#         item.apply_discount(1.5)
